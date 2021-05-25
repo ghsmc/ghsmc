3102339e5d19d80ad635eaca0fed7639ed2bc98d
George McCain <georgemccain88@gmail.com>
2021-05-25 14:57:45 -0400

    updated navbar links

--- index2.js
+++ index2.js
@@ -7,106 +7,112 @@ var Typer = {
     accessCount: 0,
     deniedCount: 0,
     init: function () {
-      accessCountimer = setInterval(function () {
-        Typer.updLstChr();
-      }, 500);
-      $.get(Typer.file, function (data) {
-        Typer.text = data;
-        Typer.text = Typer.text.slice(0, Typer.text.length - 1);
-      });
+        accessCountimer = setInterval(function () {
+            Typer.updLstChr();
+        }, 500);
+        $.get(Typer.file, function (data) {
+            Typer.text = data;
+            Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+        });
     },
-  
+
     content: function () {
-      return $('#console2').html();
+        return $('#console2').html();
     },
-  
+
     write: function (str) {
-      $('#console2').append(str);
-      return false;
+        $('#console2').append(str);
+        return false;
     },
-  
+
     addText: function (key) {
-      if (key.keyCode == 18) {
-        Typer.accessCount++;
-  
-        if (Typer.accessCount >= 3) {
-          Typer.makeAccess();
+        if (key.keyCode == 18) {
+            Typer.accessCount++;
+
+            if (Typer.accessCount >= 3) {
+                Typer.makeAccess();
+            }
+        } else if (key.keyCode == 20) {
+            Typer.deniedCount++;
+
+            if (Typer.deniedCount >= 3) {
+                Typer.makeDenied();
+            }
+        } else if (key.keyCode == 27) {
+            Typer.hidepop();
+        } else if (Typer.text) {
+            var cont = Typer.content();
+            if (cont.substring(cont.length - 1, cont.length) == '|')
+                $('#console2').html(
+                    $('#console2')
+                        .html()
+                        .substring(0, cont.length - 1),
+                );
+            if (key.keyCode != 8) {
+                Typer.index += Typer.speed;
+            } else {
+                if (Typer.index > 0) Typer.index -= Typer.speed;
+            }
+            var text = Typer.text.substring(0, Typer.index);
+            var rtn = new RegExp('\n', 'g');
+
+            $('#console2').html(text.replace(rtn, '<br/>'));
+            window.scrollBy(0, 50);
         }
-      } else if (key.keyCode == 20) {
-        Typer.deniedCount++;
-  
-        if (Typer.deniedCount >= 3) {
-          Typer.makeDenied();
+
+        if (key.preventDefault && key.keyCode != 122) {
+            key.preventDefault();
         }
-      } else if (key.keyCode == 27) {
-        Typer.hidepop();
-      } else if (Typer.text) {
-        var cont = Typer.content();
-        if (cont.substring(cont.length - 1, cont.length) == '|')
-          $('#console2').html(
-            $('#console2')
-              .html()
-              .substring(0, cont.length - 1),
-          );
-        if (key.keyCode != 8) {
-          Typer.index += Typer.speed;
-        } else {
-          if (Typer.index > 0) Typer.index -= Typer.speed;
+
+        if (key.keyCode != 122) {
+            // otherway prevent keys default behavior
+            key.returnValue = false;
         }
-        var text = Typer.text.substring(0, Typer.index);
-        var rtn = new RegExp('\n', 'g');
-  
-        $('#console2').html(text.replace(rtn, '<br/>'));
-        window.scrollBy(0, 50);
-      }
-  
-      if (key.preventDefault && key.keyCode != 122) {
-        key.preventDefault();
-      }
-  
-      if (key.keyCode != 122) {
-        // otherway prevent keys default behavior
-        key.returnValue = false;
-      }
     },
-  
-  
+
+
     updLstChr: function () {
-      var cont = this.content();
-  
-      if (cont.substring(cont.length - 1, cont.length) == '|')
-        $('#console2').html(
-          $('#console2')
-            .html()
-            .substring(0, cont.length - 1),
-        );
-      else this.write('|'); // else write it
+        var cont = this.content();
+
+        if (cont.substring(cont.length - 1, cont.length) == '|')
+            $('#console2').html(
+                $('#console2')
+                    .html()
+                    .substring(0, cont.length - 1),
+            );
     },
-  };
-  
-  function replaceUrls(text) {
+};
+
+function replaceUrls(text) {
     var http = text.indexOf('http://');
     var space = text.indexOf('.me ', http);
-  
+
     if (space != -1) {
-      var url = text.slice(http, space - 1);
-      return text.replace(url, '<a href="' + url + '">' + url + '</a>');
+        var url = text.slice(http, space - 1);
+        return text.replace(url, '<a href="' + url + '">' + url + '</a>');
     } else {
-      return text;
+        return text;
     }
-  }
-  
-  Typer.speed = 3;
-  Typer.file = 'terminal.txt';
-  Typer.init();
-  
-  var timer = setInterval('t();', 30);
-  function t() {
+}
+
+Typer.speed = 3;
+Typer.file = 'terminal.txt';
+Typer.init();
+
+var timer = setInterval('t();', 30);
+function t() {
     Typer.addText({ keyCode: 123748 });
-  
+
     if (Typer.index > Typer.text.length) {
-      clearInterval(timer);
+        clearInterval(timer);
     }
-  }
-  
-  
+}
+
+function inProgress() {
+    alert("Hey there!\ Thanks for your interest. This project is still in progress; eventually, you'll be able to check it out. Until then, you'll just have to wait!");
+}
+
+
+
+
+
